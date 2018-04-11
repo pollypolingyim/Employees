b0b7f7fa198ad1369105de6d83d17ac6456a9f08
Aleksandar Krumov <alexkrumov@gmail.com>
2018-04-11 09:37:16 -0400

    Added javadoc comments

--- src/data_access/SalaryDAO.java
+++ src/data_access/SalaryDAO.java
@@ -4,11 +4,39 @@ import java.sql.Date;
 import java.util.List;
 
 import transfer_object.Salary;
-
+/**
+ * This is an interface for the Salary DAO
+ * @author Shawn Pottle
+ * @author Polly Yum
+ * @author Aleksandar Krumov
+ */
 public interface SalaryDAO {
+	/**
+	 * This method is used to retrieve a list all salaries.
+	 * @return List of generic type <Employee>
+	 */
 	public List<Salary> getAllSalaries();
+	/**
+	 * This method is used to add salary to the salary table.
+	 * @param emp_no of type integer
+	 * @param salary of type integer
+	 * @param from_date of reference type Date
+	 * @param to_date of reference type Date
+	 */
 	public void addSalary(int emp_no, int salary, Date from_date, Date to_date);
+	/**
+	 * This method is used to update the salary table.
+	 * @param emp_no of type integer
+	 * @param salary of type integer
+	 * @param from_date of reference type Date
+	 * @param to_date of reference type Date
+	 */
 	public void updateSalary(int emp_no, int salary, Date from_date, Date to_date);
+	/**
+	 * This method is used to delete from the salary table.
+	 * @param emp_no of type integer
+	 * @param from_date of reference type Date
+	 */
 	public void deleteSalary(int emp_no, Date from_date);
 	//TODO: add deleteSalaryBy...
 
